b7ec22589baae3218df8f73871b2da217e3396e3
Florian <info@ftws.de>
2020-04-27 13:01:31 +0200

    add tgr-subscription

---
+++ tgr-subscription/tgr/_contextAggregate_/__handler.test.js
@@ -0,0 +1,56 @@
+// -------------------------------------------------------------------------
+// ALICE-RUNTIME:TEMPLATE:tgr-default:VERSION:0.1.0
+// -------------------------------------------------------------------------
+const dc = require("node-dev-console");
+const expect = require('chai').expect;
+const aliceTestEnv = require("alice-runtime/test-environment");
+const testHandler = require('./handler');
+
+dc.activate();
+
+
+describe('app/_THIS_CONTEXT_/_THIS_AGGREGATE_/trg/_contextAggregate_/handler', function () {
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    before(async () => {
+        // aliceTestEnv.loadRuntimeConfig(__dirname, "unit-test");
+        // await aliceTestEnv.connect();
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    after(async () => {
+        // await aliceTestEnv.disconnect();
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    beforeEach(async function () {
+        dc.l("---------------", this.currentTest.title, "---------------");
+        // await aliceTestEnv.clearDatabase();
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('happy path', async function () {
+        const env = aliceTestEnv.triggerTestEnvironment({
+            dryRun: true
+        });
+
+        const result = await testHandler({
+            aggregateId: "_THIS_ITEM_ID_FIELD_=value"
+        }, env);
+
+        dc.j(result, "Test result");
+        env.log.displayAll();
+
+        expect(result).to.equal(true);
+        expect(env.log.reInvokeSubscription()).to.deep.equal([
+            {
+                "context": "_THIS_CONTEXT_",
+                "aggregate": "_THIS_AGGREGATE_",
+                "command": "_THIS_COMMAND_",
+                "subscription": "_PARENT_AGGREGATE_?_THIS_ITEM_ID_FIELD_=value"
+            }
+        ]);
+    });
+
+
+});
---
+++ tgr-subscription/tgr/_contextAggregate_/handler.js
@@ -0,0 +1,33 @@
+// -------------------------------------------------------------------------
+// ALICE-RUNTIME:TEMPLATE:tgr-subscription:VERSION:0.1.0
+// -------------------------------------------------------------------------
+const dc = require("node-dev-console");
+const {utility: u} = require("alice-runtime");
+const R = require("ramda");
+
+
+const _contextAggregate__Trigger = async (event, env) => {
+    // dc.j(event, "event");
+
+    const {_THIS_ITEM_ID_FIELD_} = u.parseAggregateId(event);
+    // dc.t(_THIS_ITEM_ID_FIELD_, "_THIS_ITEM_ID_FIELD_");
+    // dc.t(_THIS_ITEM_ID_FIELD_2_, "_THIS_ITEM_ID_FIELD_2_");
+
+    // use this notation - if more only 1 id field exists
+    const aggregateId = u.stringifyId("_THIS_ITEM_ID_FIELD_", _THIS_ITEM_ID_FIELD_);
+    // use this notation - if more than 1 id field exists
+    // const aggregateId = u.stringifyId(["_THIS_ITEM_ID_FIELD_", "_THIS_ITEM_ID_FIELD_2_"], {_THIS_ITEM_ID_FIELD_, _THIS_ITEM_ID_FIELD_2_});
+    // dc.t(aggregateId, "aggregateId");
+
+    await env.reInvokeSubscription({
+        context: "_THIS_CONTEXT_",
+        aggregate: "_THIS_AGGREGATE_",
+        command: "_THIS_COMMAND_",
+        subscription: `_PARENT_AGGREGATE_?${aggregateId}`,
+    });
+
+    return true;
+};
+
+
+module.exports = _contextAggregate__Trigger;
